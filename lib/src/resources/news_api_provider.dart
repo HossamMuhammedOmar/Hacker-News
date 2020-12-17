4167a8b3b2ce9deb1187a8e5c0bdca56fe05fe60
Your Name <hossamomar9876@gmail.com>
2020-12-17 09:39:59 +0200

    create news_api_provider class

---
+++ lib/src/resources/news_api_provider.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:Hacker_News/src/models/item_model.dart';
+import 'package:http/http.dart' as http;
+
+class NewsApiProvider {
+  final String _url = "https://hacker-news.firebaseio.com/v0";
+
+  fetchTopIds() async {
+    final response = await http.get("$_url/topstories.json");
+    final listIds = json.decode(response.body);
+    return listIds;
+  }
+
+  fetchItem(int id) async {
+    final response = await http.get("$_url/item/$id.json");
+    final parsedJson = json.decode(response.body);
+    return ItemModel.fromJson(parsedJson);
+  }
+}
